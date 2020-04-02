e75bcf47320971311343e79e3e26298d4a68309b
Orest Khomitskyi <orest.khomitskyi@kindgeek.com>
2020-04-02 13:27:16 +0300

    d

--- main.c
+++ main.c
@@ -26,12 +26,63 @@
   */
 
 /* Includes ------------------------------------------------------------------*/
+
 #include "stm32f4x7_eth_bsp.h"
 #include "netconf.h"
-#include "main.h"
 #include "tcp_echoserver.h"
 #include "serial_debug.h"
 #include <stdio.h>
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "stm32f4xx.h"
+#include "stm32f4x7_eth_bsp.h"
+
+#define USE_LCD /* enable LCD  */
+
+#define MAC_ADDR0 2
+#define MAC_ADDR1 0
+#define MAC_ADDR2 0
+#define MAC_ADDR3 0
+#define MAC_ADDR4 0
+#define MAC_ADDR5 0
+
+#define IP_ADDR0 192
+#define IP_ADDR1 168
+#define IP_ADDR2 0
+#define IP_ADDR3 10
+
+#define NETMASK_ADDR0 255
+#define NETMASK_ADDR1 255
+#define NETMASK_ADDR2 255
+#define NETMASK_ADDR3 0
+
+#define GW_ADDR0 192
+#define GW_ADDR1 168
+#define GW_ADDR2 0
+#define GW_ADDR3 1
+
+#define MII_MODE
+
+/* Uncomment the define below to clock the PHY from external 25MHz crystal (only for MII mode) */
+#ifdef MII_MODE
+#define PHY_CLOCK_MCO
+#endif
+
+  void Time_Update(void);
+  void Delay(uint32_t nCount);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
 
 #define SYSTEMTICK_PERIOD_MS 10
 #define MESSAGE2 "  STM32F-4 Series   "
